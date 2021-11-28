a0dbcb0a95a1b45d9f26d797ab12d19f07e9253d
haska12 <88626706+haska12@users.noreply.github.com>
2021-11-28 23:09:09 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+let nav = document.getElementById("nuv")[0]
+let taglle = document.getElementsByClassName("taglle")[0]
+taglle.addEventListener('click', () => {
+    nav.classList.toggle('active')
+  })
